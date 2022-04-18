86d936b9b07a8b7ec0047eb28b910b9cc2d3b38e
Mark-DSouza <markdsouza434@gmail.com>
2022-04-18 09:11:06 +0530

    add read operation (CRUD) for single post entity using type-graphql

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,20 @@
 import { Post } from "./../entities/Post";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
 import { MyContext } from "src/types";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx()
+    { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
