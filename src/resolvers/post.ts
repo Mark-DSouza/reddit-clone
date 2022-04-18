95b697b096a3a8e4016489c922690eefd3d2c58f
Mark-DSouza <markdsouza434@gmail.com>
2022-04-18 10:03:22 +0530

    add delete operation (CRUD) for deleting post entity using type-graphql

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -46,4 +46,16 @@ export class PostResolver {
     }
     return post;
   }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id", () => Int) id: number,
+    @Ctx()
+    { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
+    // you can put a try catch block if you're bothered
+    // the catch block will return false
+  }
 }
